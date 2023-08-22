7546422199a082e07da9b458875828b06543839f
Arafetki <93081181+Arafetki@users.noreply.github.com>
2023-08-22 18:05:51 +0100

    first react demo (#3)
    * first demo
    
    * Updated App component to be a functional component
    
    * updated App.js
    
    * updated Readme

---
+++ monster-rolodex/README.md
@@ -0,0 +1,7 @@
+# Monsters-rolodex 
+
+This is my first react app demo 
+
+In this demo I learned how to write class components and functional components and understood the difference between them
+
+I also learned how react renders and re-renders components and how to use the useState and useEffect hooks in functional components
---
+++ monster-rolodex/src/App.js
@@ -0,0 +1,44 @@
+import { useEffect,useState } from 'react';
+import CardList from './components/card-list/CardList';
+import SearchBox from './components/search-box/SearchBox';
+
+import './App.css';
+
+
+const App = ()=>{
+
+  const [searchString,setSearchString]=useState('')
+  const [monsters,setMonsters]=useState([])
+  const [filteredMonsters,setFilteredMonsters]=useState(monsters)
+
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res)=>res.json())
+    .then((users)=>{
+      setMonsters(users)
+    })
+    .catch((err)=>console.error(err))
+  },[])
+
+  useEffect(()=>{
+    const newFilteredMonsters = monsters.filter((monster)=>{
+      return monster.name.toLowerCase().includes(searchString.toLowerCase())
+    })
+    setFilteredMonsters(newFilteredMonsters)   
+  },[monsters,searchString])
+
+  const onSearchStringChangeHandler = (event) =>{
+    setSearchString(event.target.value)
+  }
+
+
+  return (
+    <div className="App">
+        <SearchBox onChangeHandler={onSearchStringChangeHandler} placeHolder="search monsters" className='search-box'/>
+        <CardList monsters={filteredMonsters}/>
+    </div>       
+  )
+}
+
+
+export default App;
